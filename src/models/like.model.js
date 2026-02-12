3c06b5ef9d229c8387934e3233f850cf649aba2f
Abu Sayed <sayedemon53@gmail.com>
2026-02-12 20:03:25 +0600

    feat: Add Like, Playlist, Tweet, and Comment Mongoose models for backend data structures.

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    likedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    tweet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet"
+    }
+
+}, { timestamps: true })
+
+
+export const Like = mongoose.model("Like", likeSchema)
